c4292e476089e9168b482a911502e83af302a913
Mason Orsak <masonorsak@hotmail.com>
2021-02-08 23:18:40 -0600

    Initial Commit

---
+++ SeniorProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeniorProject
+//
+//  Created by Mason Orsak on 2/8/21.
+//  Copyright © 2021 Mason Orsak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
